f4b6439af57aa9a4f53bbe5b09c3cb30d9b4d9d4
Josh Davis <joshuadavis42@gmail.com>
2012-11-07 01:10:29 -0600

    Improved openl.py.
    I wrote it a longggg time ago, you can tell my Python was pretty horrid
    back then. xD

--- Scripts/openl.py
+++ Scripts/openl.py
@@ -2,52 +2,83 @@
 import os
 import sys
 
-def toInt(x):
-	try:
-		x = int(x)
-	except ValueError:
-		x = -1
-	return x
+#
+# A nice little script to check the output of a command to see if each line is
+# a valid file path. Then it will format it into an easy way to open each of
+# the paths.
+#
+# For example:
+#   > locate .gitconfig | openl.py
+#
+#   [0]    cancel
+#   [1] all
+#   [2] /Users/Davis/.gitconfig
+#   Which file(s) to open?:
+#
 
-if (len(sys.argv) > 1):
-	try:
-		print sys.argv[1]
-		std = open(sys.argv[1])
-	except:
-		print "File not found"
-else:
-	std=sys.stdin
+#
+# TODO:
+#   Add docopt module
+#   Add better path resolving
+#   Change to run arguments (not accept from stdin)
+#
 
 
-file = std.readlines()
-lines = []
-sys.stdin=open("/dev/tty")
-n = 1
+def launch(line):
+    os.system('open "{0}"'.format(line))
 
-for line in file:
-	line = line.rstrip('\n')
-	if (os.path.exists(line)):
-		if (n == 1):
-			print "[0]\tcancel\n[1]	all"
-		n += 1
-		print "[%i]\t%s" % (n, line)
-		lines.append(line)
-if not(len(lines)):
-	print "No files"
-	exit(1)
 
-x = toInt(raw_input("Which file(s) to open?: "))
+def main():
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        try:
+            print file_name
+            reader = open(file_name)
+        except:
+            print 'File {0} not found'.format(file_name)
+            return 1
+    else:
+        reader = sys.stdin
 
-while True:
-	if (x >= 0 and x <= n):
-		if (x == 0):
-			break
-		elif (x == 1):
-			for i in range(0, n - 1):
-				os.system("open \"%s\"" % lines[i])
-			break
-		else:
-			os.system("open \"%s\"" % lines[x - 2])
-			break
-	else:
-		x = toInt(raw_input("Please enter values in the range 0 through %i: " % n))
+    input_lines = reader.readlines()
+    reader.close()
+
+    sys.stdin = open('/dev/tty')
+
+    lines = []
+
+    for n, line in enumerate(input_lines):
+        line = line.rstrip('\n')
+        if os.path.exists(os.path.abspath(line)):
+            if n == 0:
+                print '[0]\tcancel\n[1]	all'
+
+            print '[{0}]\t{1}'.format(n + 2, line)
+
+            lines.append(line)
+
+    if len(lines) == 0:
+        print 'No files found in the input.'
+        return 1
+
+    to_open = raw_input('Which file(s) to open?: ').split(',')
+
+    while True:
+        if '1' in to_open:
+            for line in lines:
+                launch(line)
+        else:
+            for i in to_open:
+                try:
+                    i = int(i)
+                except:
+                    print 'Exception'
+                    continue
+
+                if 0 < i < len(lines) + 1:
+                    launch(lines[i - 2])
+
+        return 1
+
+if __name__ == '__main__':
+    sys.exit(main())
