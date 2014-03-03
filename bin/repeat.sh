50e29fe2c7fab50bb21b6c5b64d0849579b05fbe
Josh Davis <josh@joshldavis.com>
2014-03-02 18:25:34 -0600

    Use spaces, not tabs

--- bin/repeat.sh
+++ bin/repeat.sh
@@ -5,18 +5,18 @@
 # over, and over, and over, and over, until the interval runs out.
 
 if [ $# -lt 3 ]; then
-	echo Usage: repeat [iterations] [time interval] [command ... args]
-	exit
+    echo Usage: repeat [iterations] [time interval] [command ... args]
+    exit
 fi
 
 
 for ((i=3; i<$#+1; i++))
 do
-	s="${s} ${!i}"
+    s="${s} ${!i}"
 done
 echo $s
 for ((i=0; i<$1; i++))
 do
-	$s
-	sleep $2
+    $s
+    sleep $2
 done
